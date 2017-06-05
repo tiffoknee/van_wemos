1270ebb9c32661cb4b527174d2f9c1e9cb4fed47
Tiff Leek <tiffanyleek@tiffair.local>
2017-06-05 17:53:18 +0100

    write LED button control, influx db code added
    TEST influx code

--- van_wemos.ino
+++ van_wemos.ino
@@ -3,6 +3,7 @@
 // Van Automation
 //
 
+#include <SPI.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
@@ -24,6 +25,17 @@ const int OFF = 0;
 const int ON = 1;
 const int AUTO = 2;
 
+const int ledsDOWN = 0;
+const int ledsUP  = 1;
+
+int status = WL_IDLE_STATUS;     // the Wifi radio's status
+
+// the IP address of the InfluxDB host
+byte host[] = {88, 80, 191, 61};
+
+// the port that the InfluxDB UDP plugin is listening on
+int port = 8888;
+
 #define ledPin D6
 #define dhtIntInPin D4
 #define dhtIntOutPin D3
@@ -35,15 +47,32 @@ const int AUTO = 2;
 #define fanButtonPin D8 // momentary press button, state machine
 #define ledButtonPin D7 // press button, state machine with hold
 
-int fanState = OFF; // 0 == off, 1 == on, 2 == auto
+int currentR = 127;
+int currentG = 127;
+int currentB = 127;
+int fanControlState = AUTO; // 0 == off, 1 == on, 2 == auto
+int fanState = AUTO;
 
-unsigned long ledButtonMillis = 0;
+int activeLEDs = 0; //number of LEDs currently active
+int secondCount = 0; //don't change the active led if it's already been changed this second
+
+int showColour = 0;
 unsigned long dhtLastCheck = 0;
 unsigned long fanButtonLastPressed = 0;
 unsigned long wifiLastCheck = 0;
 
-long dhtInterval = 6000;           // milliseconds between sensor checks
-long checkForWifiInterval = 6000;  //milliseconds between checks for wifi
+int prevLEDs = ledsDOWN; // 0 = previous LEDs ledsDOWN, 1 = previous LEDs ledsUP
+
+int current;         // Current state of the button
+// (LOW is pressed b/c i'm using the pullup resistors)
+long millis_held;    // How long the button was held (milliseconds)
+long secs_held;      // How long the button was held (seconds)
+long prev_secs_held; // How long the button was held in the previous check
+byte previous = HIGH;
+unsigned long firstTime; // how long since the button was first pressed
+
+long dhtInterval = 30000;           // milliseconds between sensor checks
+long checkForWifiInterval = 30000;  //milliseconds between checks for wifi
 
 float maxTemp = 23.00;
 float maxHumidity = 80.00;
@@ -87,20 +116,24 @@ void setup() {
 
   //DHT setup
   delay(6000);           //wait 6 seconds
-  Serial.println("Temperature and Humidity test!");//print on Serial monitor
-  Serial.println("T(C) \tH(%)");  //print on Serial monitor
+  //  Serial.println("Temperature and Humidity test!");//print on Serial monitor
+  //  Serial.println("T(C) \tH(%)");  //print on Serial monitor
 
   dhtIntIn.begin();
   dhtExtIn.begin();
   dhtIntOut.begin();
   dhtExtOut.begin();          //.. initialize the Serial communication
+
   //end DHT setup
 
   // LEDs
   pixels.begin(); // This initializes the NeoPixel library.
+
+  pixels.show();
+
   // END LEDS
 
-  
+
 
 }
 
@@ -108,8 +141,113 @@ void setup() {
 void loop() {
 
 
+
+  String lineSend = "";
   unsigned long currentMillis = millis();
   int val;
+  String now = String(timeClient.getEpochTime());
+
+  //handle the LED button
+
+  current = digitalRead(ledButtonPin);
+
+  // if the button state changes to pressed, remember the start time
+  if (current == HIGH && previous == LOW && (millis() - firstTime) > 200) {
+    firstTime = millis();
+  }
+
+  millis_held = (millis() - firstTime);
+  secs_held = millis_held / 1000;
+
+  // This if statement is a basic debouncing tool, the button must be pushed for at least
+  // 100 milliseconds in a row for it to be considered as a push.
+  if (millis_held > 50) {
+
+    if (current == HIGH && secs_held > prev_secs_held) {
+      Serial.print("1 prevLEDs = ");
+      Serial.println(prevLEDs);
+      if (secondCount != secs_held) {
+
+        if (prevLEDs == ledsUP  && activeLEDs > 0) { //decrease number
+
+          if (activeLEDs != 0) {
+            setColor(activeLEDs - 1, 0, 0, 0, 50);
+            Serial.println("setColor(" + String(activeLEDs - 1) + "0, 0, 0, 50)");
+            pixels.show();
+            activeLEDs --;
+            Serial.print("decrement active LEDs to:");
+            Serial.println(activeLEDs);
+            secondCount = secs_held;
+          }
+
+
+        } else { //increase number
+
+          if (activeLEDs == 7) {
+            colorWipe(pixels.Color(0, 0, 0), 50, activeLEDs);
+            secondCount = secs_held;
+            activeLEDs = 0;
+            Serial.print("active LEDs = 7. set to 0");
+            Serial.println(activeLEDs);
+          } else {
+
+            setColor(activeLEDs - 1, currentR, currentG, currentB, 100);
+
+            Serial.println("setColor(" + String(activeLEDs - 1) + ", 100, 255, 150, 100)");
+
+            activeLEDs ++;
+
+            Serial.print("increment active LEDs to:");
+            Serial.println(activeLEDs);
+            secondCount = secs_held;
+          }
+        }
+
+      }
+
+      Serial.println();
+      Serial.println(String(secs_held) + " + prev" + String(prev_secs_held));
+
+
+    }
+
+    // check if the button was released since we last checked
+    if (current == LOW && previous == HIGH && currentMillis > 19000) {
+
+      if (activeLEDs == 0) {
+        prevLEDs = ledsDOWN; //decrease
+      } else {
+        if (prevLEDs == ledsDOWN) {
+          prevLEDs = ledsUP ; //increase
+        } else {
+          prevLEDs = ledsDOWN ; //increase
+        }
+
+      }
+
+      // Button pressed for less than 1 second, change colour
+      if (secs_held <= 0) {
+        showColour++;
+        if (showColour > 4)
+          showColour = 0;
+        cycleColour(showColour);
+         colorWipe(pixels.Color(currentR, currentG, currentB), 5, activeLEDs - 1);
+      }
+
+
+    }
+  }
+
+  previous = current;
+  prev_secs_held = secs_held;
+
+  //end handle LED button
+
+  if (WiFi.status() == WL_CONNECTED) {
+    lineSend += ("epoch_time value=" + now); // build a string to post to the influxdb
+    //Serial.println(lineSend);
+  }
+
 
   //check and see if there's available wifi..
   if ((WiFi.status() != WL_CONNECTED) && (currentMillis - wifiLastCheck >= checkForWifiInterval)) {
@@ -121,10 +259,9 @@ void loop() {
 
   if (val == HIGH && (currentMillis - fanButtonLastPressed >= 600)) {  // check if the input is LOW (button pressed) and debounce
 
-    fanButtonLastPressed = currentMillis;
-    //is the button momentary click? I think so!
 
-    if (fanState == OFF) {
+    if (fanControlState == OFF) {
+      fanControlState = ON;
       fanState = ON;
       digitalWrite(inFanPin, HIGH);
       digitalWrite(outFanPin, HIGH);
@@ -133,20 +270,23 @@ void loop() {
       {
         setColor(led, 0, 255, 0, 100); //green
       }
+      colorWipe(pixels.Color(currentR, currentG, currentB), 5, activeLEDs - 1);
+
 
     }
 
-    else if (fanState == ON) {
-      fanState = AUTO;
+    else if (fanControlState == ON) {
+      fanControlState = AUTO;
       Serial.println("FAN - AUTO");
       for (led = 0; led <= 6; led++)
       {
         setColor(led, 0, 0, 255, 100); //blue
       }
-
+      colorWipe(pixels.Color(currentR, currentG, currentB), 5, activeLEDs - 1);
     }
 
-    else if (fanState == AUTO) {
+    else if (fanControlState == AUTO) {
+      fanControlState = OFF;
       fanState = OFF;
       digitalWrite(inFanPin, LOW);
       digitalWrite(outFanPin, LOW);
@@ -155,74 +295,48 @@ void loop() {
       {
         setColor(led, 255, 0, 0, 100); //blue
       }
+      colorWipe(pixels.Color(currentR, currentG, currentB), 5, activeLEDs - 1);
 
     }
+    fanButtonLastPressed = currentMillis;
+    //is the button momentary click? I think so!
   }
 
 
   if ((currentMillis - dhtLastCheck >= dhtInterval) ||
-      (fanButtonLastPressed == currentMillis && fanState == AUTO)) { //check every dhtInterval milliseconds OR if fan is set to auto
+      (fanButtonLastPressed == currentMillis && fanControlState == AUTO)) {
+    //check every dhtInterval milliseconds OR if fan is set to auto
 
-    float h = dhtIntIn.readHumidity();    // reading Humidity
-    float t = dhtIntIn.readTemperature(); // read Temperature as Celsius (the default)
-    // check if any reads failed and exit early (to try again).
-    if (isnan(h) || isnan(t)) {
-      // Serial.println("Failed to read from DHT 1 sensor!");
-      return;
-    } else {
+    lineSend += checkSensor(dhtIntIn.readHumidity(), dhtIntIn.readTemperature(), "intIn");
+    lineSend += checkSensor(dhtIntOut.readHumidity(), dhtIntOut.readTemperature(), "intOut");
+    lineSend += checkSensor(dhtExtIn.readHumidity(), dhtExtIn.readTemperature(), "extIn");
+    lineSend += checkSensor(dhtExtOut.readHumidity(), dhtExtOut.readTemperature(), "extOut");
 
-      Serial.print(t, 2);    //print the temperature
-      Serial.print("\t");
-      Serial.println(h, 2);  //print the humidity
-    }
-    h = dhtIntOut.readHumidity();    // reading Humidity
-    t = dhtIntOut.readTemperature(); // read Temperature as Celsius (the default)
-    // check if any reads failed and exit early (to try again).
-    if (isnan(h) || isnan(t)) {
-      // Serial.println("Failed to read from DHT 1 sensor!");
-      return;
-    } else {
-
-      Serial.print(t, 2);    //print the temperature
-      Serial.print("\t");
-      Serial.println(h, 2);  //print the humidity
-    }
-    h = dhtExtIn.readHumidity();    // reading Humidity
-    t = dhtExtIn.readTemperature(); // read Temperature as Celsius (the default)
-    // check if any reads failed and exit early (to try again).
-    if (isnan(h) || isnan(t)) {
-      // Serial.println("Failed to read from DHT 1 sensor!");
-      return;
-    } else {
-
-      Serial.print(t, 2);    //print the temperature
-      Serial.print("\t");
-      Serial.println(h, 2);  //print the humidity
-    }
-    h = dhtExtOut.readHumidity();    // reading Humidity
-    t = dhtExtOut.readTemperature(); // read Temperature as Celsius (the default)
-    // check if any reads failed and exit early (to try again).
-    if (isnan(h) || isnan(t)) {
-      // Serial.println("Failed to read from DHT 1 sensor!");
-      return;
-    } else {
-
-      Serial.print(t, 2);    //print the temperature
-      Serial.print("\t");
-      Serial.println(h, 2);  //print the humidity
-    }
-    if (fanState == AUTO) { //if fan is set to auto, check the temp and humidity
-      if ((t >= maxTemp || h >= maxHumidity)) {
+    if (fanControlState == AUTO) { //if fan is set to auto, check the temp and humidity
+      if ((dhtIntOut.readTemperature() >= maxTemp || dhtIntOut.readHumidity() >= maxHumidity)) {
         digitalWrite(inFanPin, HIGH);
         digitalWrite(outFanPin, HIGH);
         Serial.println("auto - ON");
+        fanState = ON;
       } else {
         digitalWrite(inFanPin, LOW);
         digitalWrite(outFanPin, LOW);
         Serial.println("auto - OFF");
+        fanState = OFF;
       }
     }
 
+
+
+
+    if (WiFi.status() == WL_CONNECTED) {
+      lineSend += ", fans value=" + String(fanState);
+      Serial.println(lineSend);
+
+
+      Serial.println(" send data (disabled)");
+      //sendData(lineSend); //send the data
+    }
 
     dhtLastCheck = currentMillis; //reset dhtLastCheck
   }
@@ -251,15 +365,15 @@ void loop() {
   if (request.indexOf("/FAN=ON") != -1) {
     digitalWrite(inFanPin, HIGH);
     digitalWrite(outFanPin, HIGH);
-    fanState = ON;
+    fanControlState = ON;
   }
   if (request.indexOf("/FAN=OFF") != -1) {
     digitalWrite(inFanPin, LOW);
     digitalWrite(outFanPin, LOW);
-    fanState = OFF;
+    fanControlState = OFF;
   }
   if (request.indexOf("/FAN=AUTO") != -1) {
-    fanState = AUTO;
+    fanControlState = AUTO;
   }
 
   // Return the response
@@ -271,9 +385,9 @@ void loop() {
 
   client.print("Fan state is now: ");
 
-  if (fanState == ON) {
+  if (fanControlState == ON) {
     client.print("On");
-  } else if (fanState == OFF) {
+  } else if (fanControlState == OFF) {
     client.print("Off");
   } else {
     client.print("Off");
@@ -297,6 +411,50 @@ void setColor(int led, int redValue, int greenValue, int blueValue, int delayVal
   pixels.setPixelColor(led, pixels.Color(redValue, greenValue, blueValue));
   pixels.show();
   delay(delayValue);
+}
+
+int cycleColour(int i) {
+  switch (i) {
+    case 0:  // warm white
+      currentR = 127;
+      currentG = 100;
+      currentB = 80;
+      break;
+    case 1:  // glowy dim
+      currentR = 60;
+      currentG = 30;
+      currentB = 10;
+      break;
+
+    case 2:  // Red
+      currentR = 180;
+      currentG = 0;
+      currentB = 0;
+      break;
+
+    case 3:   // dark blue
+      currentR = 0;
+      currentG = 0;
+      currentB = 100;
+      break;
+      
+
+    case 4:   // yellow
+      currentR = 200;
+      currentG = 100;
+      currentB = 0;
+      break;
+
+  }
+}
+
+// Fill the dots one after the other with a color
+void colorWipe(int c, char wait, int n) {
+  for (short i = 0; i < n; i++) {
+    pixels.setPixelColor(i, c);
+    pixels.show();
+    delay(wait);
+  }
 }
 
 void joinWifi() {
@@ -346,4 +504,55 @@ void joinWifi() {
     }
   }
 }
+
+void sendData(String s) {
+  // send the packet
+  Serial.println("Sending UDP packet...");
+  ntpUDP.beginPacket(host, port);
+  ntpUDP.print(s);
+  ntpUDP.endPacket();
+}
+
+String checkSensor(float h, float t, String sensor) {
+
+  // check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    // Serial.println("Failed to read from "+sensor+"sensor!");
+    return "";
+  } else {
+    // concatenate the temperature into the line protocol
+    //Serial.println(","+sensor + "Humidity value=" + String(h) + ", "+ sensor +"Temperature value=" + String(t));
+
+    return "," + sensor + "Humidity value=" + String(h) + ", " + sensor + "Temperature value=" + String(t);
+  }
+
+}
+
+void rainbow(char wait) {
+  short i, j;
+
+  for (j = 0; j < 256; j++) {
+    for (i = 0; i < pixels.numPixels(); i++) {
+      pixels.setPixelColor(i, Wheel((i + j) & 255));
+    }
+    pixels.show();
+    delay(wait);
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+int Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if (WheelPos < 85) {
+    return pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if (WheelPos < 170) {
+    WheelPos -= 85;
+    return pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
 
